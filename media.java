eed278a50fcc71e6c0784f773e2ecb3e6e2a197f
Ayejuni Ilemobayo <kingsilemobayo@gmail.com>
2017-04-19 15:59:34 +0100

    meadia

---
+++ media.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+import javax.media.*;
+
+public class MediaPlayerDemo extends JFrame {
+  public static void main(String args[]) {
+    Player player;
+
+    File file = new File("yourFile");
+
+    player = Manager.createPlayer(file.toURI().toURL());
+//    player.addControllerListener(new EventHandler());
+    player.start(); // start player
+    
+    player.close();
+
+    Component visual = player.getVisualComponent();
+    Component control = player.getControlPanelComponent();
+
+  }
+}
+
+   
